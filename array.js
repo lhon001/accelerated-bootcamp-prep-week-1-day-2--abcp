561dcdb395054328ff4e5472686f8e01d1786ec4
Lawrence Hon <lhon001@gmail.com>
2018-01-14 17:56:56 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,20 +1,21 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
- 
+ return [element, ...aray]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
   array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
-
+  return array
 }
 
 function accessElementInArray(array, index) {
